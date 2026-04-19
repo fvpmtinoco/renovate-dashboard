be49a046d65bd9557a8245d6ed3e9d512da3b378
fernando.tinoco <fernando.tinoco@neuraspace.com>
2026-04-19 19:49:55 +0100

    refactor: remove unused IConfiguration from GitLabService stub

--- src/RenovateDashboard.App/Services/GitLabService.cs
+++ src/RenovateDashboard.App/Services/GitLabService.cs
@@ -4,12 +4,10 @@ namespace RenovateDashboard.App.Services;
 public class GitLabService
 {
     private readonly HttpClient _http;
-    private readonly IConfiguration _config;
 
-    public GitLabService(HttpClient http, IConfiguration config)
+    public GitLabService(HttpClient http)
     {
         _http = http;
-        _config = config;
     }
 
     public Task<IEnumerable<object>> GetRenovateMrsAsync() =>
